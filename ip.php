77cdff2c091f721e838ba8f1fb2f5d25ea10c245
grotoScript <32337192+grotoScript@users.noreply.github.com>
2017-09-27 13:17:03 -0300

    ip.php
    ff

---
+++ ip.php
@@ -0,0 +1,10 @@
+<html>
+<im>
+<?php
+
+$ip = $_SERVER['REMOTE_ADDR'];
+
+echo $ip;
+//esse é um sistema onde pode ser usado para extrair ip do client no VB.net, C#, C++ e outros, Basta colocar um download string :D
+?>
+</html>
